bb28c905b3db24dfaf702bce393f92ea8ba29e61
Magda Sloninska <magdalena.sloninska@gmail.com>
2023-02-07 17:21:37 +0100

    Drawing the players' moves dynamically

--- static/reversi_gameplay.js
+++ static/reversi_gameplay.js
@@ -9,7 +9,9 @@ initBoard = [[0, 0, 0, 0, 0, 0, 0, 0],
 
 const dirs = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]];
 
-function checkCell(board, cellRow, cellCol, player) {
+const squareSize = 60;
+
+function checkCell(cellRow, cellCol) {
     // first recognize what the player clicked
     // see if this cell can be selected according to game rules
 
@@ -34,7 +36,7 @@ function checkCell(board, cellRow, cellCol, player) {
     return true;
 }
 
-function selectCell(board, cellRow, cellCol, player) {
+function selectCell(cellRow, cellCol) {
     // if the cell is can be selected we:
     //  - draw an appropriate circle
     //  - flip the other discs
@@ -42,16 +44,11 @@ function selectCell(board, cellRow, cellCol, player) {
 
     console.log(`Selected cell: ${cellRow}, ${cellCol}`);
 
-    gameState.board[cellRow][cellCol] = player;
+    gameState.board[cellRow][cellCol] = gameState.player;
 }
 
-function drawBoard(board, player) {
+function drawBoard() {
     // ideally we would like to move the script for drawing the board here so that both attack & defence can use it
-
-    const squareSize = 60;
-
-    let canvas = document.getElementById('canvasBoard');
-    context = canvas.getContext('2d');
 
     for (let row = 0; row < 8; row++) {
         for (let col = 0; col < 8; col++) {
@@ -64,10 +61,10 @@ function drawBoard(board, player) {
 
     for (let row = 0; row < 8; row++) {
         for (let col = 0; col < 8; col++) {
-            if (board[row][col] != 0) {
+            if (gameState.board[row][col] != 0) {
                 context.beginPath();
-                context.arc(col*squareSize + squareSize/2, row*squareSize + squareSize/2, 25, 0, 2*Math.PI, false);
-                context.fillStyle = (board[row][col] == -1) ? 'white' : 'black';
+                context.arc(col*squareSize + squareSize/2, row*squareSize + squareSize/2, 25, 0, 2*Math.PI);
+                context.fillStyle = (gameState.board[row][col] == -1) ? 'white' : 'black';
                 context.fill();
             }
         }
@@ -76,20 +73,28 @@ function drawBoard(board, player) {
     context.strokeStyle = 'black';
     context.strokeRect(0, 0, squareSize*8, squareSize*8)
 
-    canvas.onclick = function(event) { // TODO: figure out why this works, but addEventListener doesn't?
+    // return board;
+}
+
+function handleTurn() {
+
+    let canvas = document.getElementById('canvasBoard');
+    context = canvas.getContext('2d');
+
+    drawBoard();
+    
+    canvas.onclick = function(event) {
         // make sure the user clicked somewhere "in range"?
         let cellRow = Math.floor(event.pageY / squareSize) - 1;
         let cellCol = Math.floor(event.pageX / squareSize) - 1;
         console.log('Hello, you clicked!')
 
-        if (checkCell(board, cellRow, cellCol, player)) {
-            selectCell(board, cellRow, cellCol, player);
+        if (checkCell(cellRow, cellCol)) {
+            selectCell(cellRow, cellCol);
         }
+
+        drawBoard();
     };
-
-    return board;
 }
 
-window.onload = function() {
-    drawBoard(gameState.board, gameState.player);
-}
+window.onload = handleTurn;
